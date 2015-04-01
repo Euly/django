5bafa11cc8f5a2d1a06fc4916f2e980047e09261
Diana Sighinolfi <diana.sighinolfi@gmail.com>
2015-04-01 18:23:04 +0200

    Sistemata registrazione.

--- obelix/obelix/forms.py
+++ obelix/obelix/forms.py
@@ -4,16 +4,17 @@ from django.contrib.auth.forms import UserCreationForm
 
 class Registrazione(UserCreationForm):
 	matricola = forms.IntegerField(required=True)
-	username = forms.CharField(required=False)
+	username = forms.CharField(required=True)
 	
 	class Meta:
 		model = User
-		fields = ('first_name', 'last_name', 'matricola', 'password1', 'password2')
+		fields = ('first_name', 'last_name', 'matricola', 'username','password1', 'password2')
 
 def save(self, commit=True):
         user = super(Registrazione, self).save(commit=False)
         user.matricola = self.cleaned_data['matricola']
-        # user.set_password(self.cleaned_data['password1'])
+        user.username = self.cleaned_data['username']
+        user.set_password(self.cleaned_data['password1'])
         
         if commit:
             user.save()
