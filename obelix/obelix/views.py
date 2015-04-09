ccf9dca2ce31f0ce5b0621ddbdf37dcc598949a4
finalmax89 <77485@studenti.unimore.it>
2015-04-09 12:33:05 +0200

    registrazione quasi completa.

--- obelix/obelix/views.py
+++ obelix/obelix/views.py
@@ -76,7 +76,7 @@ def register_user(request):
 			email_subject = 'Attiva Account'
 			email_body = "Hey %s, grazie per esserti registrato.\nPer attivare il tuo account, clicca sul link seguente entro 48 ore\nhttp://127.0.0.1:8000/accounts/attivazione/%s" % (username_html, activation_key)
 
-			send_mail(email_subject, email_body, 'obelixfim@gmail.com.com', [email_html], fail_silently=False)
+			send_mail(email_subject, email_body, 'obelixfim@gmail.com', [email_html], fail_silently=False)
 
 			return HttpResponseRedirect('/accounts/register_success')
 		else :
