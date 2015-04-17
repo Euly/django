f52ef3e473bb2359c6d3bd04a567ba98f38b3f8a
Diana Sighinolfi <diana.sighinolfi@gmail.com>
2015-04-17 13:15:12 +0200

    Aggiuta titolo nella home.

--- obelix/obelix/views.py
+++ obelix/obelix/views.py
@@ -52,7 +52,7 @@ def invalid_login(request):
 @login_required
 def logout(request):
 	auth.logout(request)
-	return render_to_response('logout.html')
+	return render_to_response('home.html')
 	
 def register_user(request):
 	args = {}
