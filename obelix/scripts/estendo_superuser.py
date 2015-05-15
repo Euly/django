0124b4615c99c369dca310bd581c19564b7b713d
Diana Sighinolfi <diana.sighinolfi@gmail.com>
2015-05-15 17:31:20 +0200

    Tolta creazione doppia dello UserProfile dello superuser (era già presente grazie al dump del db).

--- obelix/scripts/estendo_superuser.py
+++ obelix/scripts/estendo_superuser.py
@@ -3,7 +3,11 @@ from django.contrib.auth.models import User
 from dispense.models import UserProfile
 from django.utils import timezone
 
-superuser = User.objects.create_superuser(username="obelix", password="dbdjob@@", email="obelix.fim@gmail.com")
-u=UserProfile(user=superuser,activation_key = "SU", key_expires=timezone.now())
-u.save()
+try:
+	superuser = User.objects.get(is_superuser=1)
+except:
+	superuser = User.objects.create_superuser(username="obelix", password="dbdjob@@", email="obelix.fim@gmail.com")
+	
+#u=UserProfile(user=superuser,activation_key = "SU", key_expires=timezone.now())
+#u.save()
   
