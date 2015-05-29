0af4a27d18e85dfc121de6277bcdfd72f8c65db3
Diana Sighinolfi <diana.sighinolfi@gmail.com>
2015-05-29 18:10:08 +0200

    Aggiornato readme.

--- obelix/sito_obelix.sh
+++ obelix/sito_obelix.sh
@@ -4,5 +4,6 @@ mysql --user=root --password= < creo_database.sql
 mysql --user=obelix --password=dbdjob@ --database=obelixdb < dump_obelixdb.sql
 python manage.py shell < scripts/estendo_superuser.py
 python manage.py syncdb
+mysql --user=root --password= < test.sql
 python manage.py runserver
 
