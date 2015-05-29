cca35152bbb347eb98a144c5604173ae10004ad6
Diana Sighinolfi <diana.sighinolfi@gmail.com>
2015-05-29 15:01:30 +0200

    Tolte piccole imperfezioni.

--- obelix/dispense/admin.py
+++ obelix/dispense/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from dispense.models import *
+from dispense.models import Corso, Insegnamento, UserProfile
 
 admin.site.register(Corso)
 admin.site.register(Insegnamento)
 admin.site.register(UserProfile)
-admin.site.register(Opinione)
